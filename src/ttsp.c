a3640c4b67b1a030aac86f7f3d9c0cd0015cb447
Madanie <madi@localhost.localdomain>
2023-11-28 13:54:54 +0200

    string testing

--- src/ttsp.c
+++ src/ttsp.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
@@ -11,52 +12,77 @@ int section = 0;
 double speed=3.0;
 int volume=100;
 
-void *toaud(void *tt){
-	printf("%s\n",(char *)tt);
+char * cleanLine(char * str){
+   char *end;
 
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+
+}
+char **splitTxt(char *file){
+   char ** ProcessedLines= malloc(sizeof(char *) * 1024);
+   int ProcessedI = 0;
 	//boring var declarations
-	FILE * fp;
+	ssize_t read;
+	char dels[3]=".,";
+	char *subline;
+
+   printf("a1\n");
+	//loop to convert each line
 	char * line = NULL;
 	size_t len = 0;
-	ssize_t read;
+	FILE * InputFile = fopen((char *)file, "r");
+	if (InputFile == NULL){
+      printf("error opening file %s \n", (char *)file);
+      return NULL;
+   }
+   while ((read = getline(&line, &len, InputFile)) != -1) {
+      if(strlen(line)<2){continue;}
+      subline = strtok(line, dels);
+      for (;subline!=NULL;subline = strtok(NULL, dels)){
+         if(strlen(subline)<2){continue;}
+         if(asprintf(&ProcessedLines[ProcessedI], cleanLine(subline)) > 1){
+            printf("%s\n",ProcessedLines[ProcessedI]);
+            ProcessedI+=1;
+         }
+
+      }
+   }
+   fclose(InputFile);
+   if (line){free(line);}
+   return ProcessedLines;
+}
+void txtToAud(void *tt){
+
+}
+void *toaud(void *tt){
+   //TODO OLD
+   char ** lines = splitTxt(tt);
+   char * l=lines[0];
+   for(int i = 0; !strcmp(l, "");i++,l=lines[i] ){
+      printf("%s\n", l);
+   }
+   free(lines);
+   return NULL;;
+
 	
-	char *subline;
-	char dels[2]=".";
 	//command to run to convert to tts
 	char ttsCommand[]="echo \"%s:.........:\" | text2wave -f 32000 -o \"ttsOut%d.wav\" %s ";
 	char ttsAppend[]="-eval '(voice_cmu_us_slt_arctic_hts)'";
 	//char ttsAppend[]="";
 	
-	//loop to convert each line
-	fp = fopen((char *)tt, "r");
-	if (fp != NULL){
-		while ((read = getline(&line, &len, fp)) != -1) {
-			if(strlen(line)<2){continue;}
-			subline = strtok(line, dels);
-			while(subline!=NULL){
-
-				if(strlen(subline)<2){
-					subline = strtok(NULL, dels);
-					continue;
-				}
-				char *s;
-				if(0 > asprintf(&s, ttsCommand,subline, section, ttsAppend)){
-					done=1;
-					printf("error converting to audio\n");
-					printf("%s\n",s);
-					return NULL;
-				}
-				//printf("%s\n",s);
-				system(s);
-				free(s);
-				section+=1;
-				subline = strtok(NULL, dels);
-			}
-		}
-		fclose(fp);
-		if (line){free(line);}
-	}else{printf("error opening file %s \n", (char *)tt);}
-	printf("\ndone--------------------------\n");
 	done=1;
 	return NULL;
 }
@@ -77,17 +103,21 @@ void speak(){
 			break;
 		}
 	}
-
+}
+void clean(){
+   //real unsafe stuff, TODO make temp folder
+	system("rm -f *ttsOut*");
 }
 
 #ifndef UNIT_TESTING
 int main(int argc, char *argv[]){
-	system("rm -f *ttsOut*");
 	pthread_t t_id;
-	if(argc!=2){
+   clean();
+   // TODO allow more arguments
+   // if one argument, assume that to be input file, otherwise use stdin
+	if(argc<2){
 		pthread_create(&t_id, NULL,toaud, "/dev/stdin\0");
 	}else{
-
 		pthread_create(&t_id, NULL,toaud, argv[1]);
 	}
 	//todo make  args
--- tests/test.c
+++ tests/test.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #define UNIT_TESTING
 #include "../src/ttsp.c"
 #include <assert.h>
 
+int M_TestReport(int fail, int succ){
+   printf("\nRan %d Tests: %d failed, %d succeeded.\n", fail+succ , fail, succ);
+   return fail;
+}
+int M_AssertString(char * a, char * b, int* fail, int * succ){
+   if(!strcmp(a, b)){
+      *succ+=1;
+      return 1;
+   }
+   *fail+=1;
+   printf ("Assert fail: [%s]!=[%s]\n",a,b);
+   return 0; 
+}
+
+int testStringin(){
+   int  f=0;
+   int  s=0;
+   system("echo \"uwu. owo\n AWO aaa. Can't, cannot\">TESTTXT");
+   char ** t = splitTxt("TESTTXT");
+   assert(t!=NULL);
+   M_AssertString(  t[0]  , "uwu"  , &f, &s);
+   M_AssertString(  t[1]  , "owo"  , &f, &s );
+   M_AssertString(  t[2]  , "AwO aaa"   , &f, &s);
+   M_AssertString(  t[3]  , "Can't"  , &f, &s);
+   //M_AssertString(  t[4]  , "cannot"  , &f, &s );
+   printf("uwu\n");
+   return M_TestReport(f, s);
+}
 int main(){
-   printf("Testing\n");
-   assert(1==1);
-   assert(1==2);
+	int fail=0,succ=0;
+	testStringin() ? fail++ : succ ++;
 }
