f4bc04340d81790476a5ee84d4691e36dd377cb0
Madanie <madi@localhost.localdomain>
2023-12-11 10:55:05 +0200

    cleanup

--- src/ttsp.c
+++ src/ttsp.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <uchar.h>
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
@@ -6,84 +7,68 @@
 #include <unistd.h>
 #include <string.h>
 
-int done=0;
-int section = 0;
-
+//command for festival text-to-speech to file
+static const char ttsCommand[] = "echo \"%s:.........:\" | text2wave -f 32000 -eval '(voice_cmu_us_slt_arctic_hts)'";
+	//char ttsCommand[]="echo \"%s:.........:\" | text2wave -f 32000 -o \"ttsOut%d.wav\" ";
 double speed=3.0;
 int volume=100;
 
-char * cleanLine(char * str){
+char * trimLine(char * str){
    char *end;
-
   // Trim leading space
   while(isspace((unsigned char)*str)) str++;
-
-  if(*str == 0)  // All spaces?
-    return str;
-
+  if(*str == 0){return str;}
   // Trim trailing space
   end = str + strlen(str) - 1;
   while(end > str && isspace((unsigned char)*end)) end--;
-
-  // Write new null terminator character
   end[1] = '\0';
-
   return str;
-
 }
 char **splitTxt(char *file){
-   char ** ProcessedLines= malloc(sizeof(char *) * 1024);
-   int ProcessedI = 0;
-	//boring var declarations
-	ssize_t read;
+	int max_lines=4096;
 	char dels[3]=".,";
-	char *subline;
-
-   printf("a1\n");
-	//loop to convert each line
+	char ** ProcessedLines= malloc(sizeof(char *) * max_lines);
+	int ProcessedI = 0;
+	ssize_t read;
+	char * subline;
 	char * line = NULL;
 	size_t len = 0;
 	FILE * InputFile = fopen((char *)file, "r");
-	if (InputFile == NULL){
-      printf("error opening file %s \n", (char *)file);
-      return NULL;
-   }
-   while ((read = getline(&line, &len, InputFile)) != -1) {
-      if(strlen(line)<2){continue;}
-      subline = strtok(line, dels);
-      for (;subline!=NULL;subline = strtok(NULL, dels)){
-         if(strlen(subline)<2){continue;}
-         if(asprintf(&ProcessedLines[ProcessedI], cleanLine(subline)) > 1){
-            printf("%s\n",ProcessedLines[ProcessedI]);
-            ProcessedI+=1;
-         }
-
-      }
-   }
-   fclose(InputFile);
-   if (line){free(line);}
-   return ProcessedLines;
+	if (InputFile == NULL){printf("error opening file %s \n", (char *)file);return NULL;}
+	while ((read = getline(&line, &len, InputFile)) != -1) {
+		if(strlen(line)<2){continue;}
+		subline = strtok(line, dels);
+		for (;subline!=NULL;subline = strtok(NULL, dels)){
+			if(strlen(subline)<2){continue;}
+			if(asprintf(&ProcessedLines[ProcessedI], trimLine(subline)) > 1){
+				ProcessedI+=1;
+				if (ProcessedI>=max_lines){break;}
+			}
+		}
+		if (ProcessedI>=max_lines){break;}
+	}
+	fclose(InputFile);
+	if (line){free(line);}
+	return ProcessedLines;
 }
-void txtToAud(void *tt){
-
+char * strToCommand(char *txt){
+	char * parcedtxt;
+	asprintf(&parcedtxt,ttsCommand ,txt);
+	return parcedtxt;;
 }
-void *toaud(void *tt){
-   //TODO OLD
-   char ** lines = splitTxt(tt);
-   char * l=lines[0];
-   for(int i = 0; !strcmp(l, "");i++,l=lines[i] ){
-      printf("%s\n", l);
-   }
-   free(lines);
-   return NULL;;
-
-	
-	//command to run to convert to tts
-	char ttsCommand[]="echo \"%s:.........:\" | text2wave -f 32000 -o \"ttsOut%d.wav\" %s ";
-	char ttsAppend[]="-eval '(voice_cmu_us_slt_arctic_hts)'";
-	//char ttsAppend[]="";
-	
+int done=0;
+int section = 0;
+FILE * temp = tmpfile();
+void *textToAudio(void *tt){
+	char ** lines = splitTxt(tt);
+	for(section = 0; lines[section];section++){
+		temp = tmpfile();
+		printf(":%s\n", lines[section]);
+		system(strToCommand(lines[section]));
+		free(lines[section]);
+	}
 	done=1;
+	free(lines);
 	return NULL;
 }
 
@@ -91,7 +76,6 @@ void speak(){
 	int sectionSpeaking=0;
 	while(1){
 		if(section>sectionSpeaking){
-			//printf("b %d >= %d\n",section,sectionSpeaking);
 			char *ss;
 			if(0 > asprintf(&ss,"mpv -ao=alsa --speed=%f --volume=%d ttsOut%d.wav",speed,volume, sectionSpeaking)){continue;}
 			sectionSpeaking+=1;
@@ -112,19 +96,14 @@ void clean(){
 #ifndef UNIT_TESTING
 int main(int argc, char *argv[]){
 	pthread_t t_id;
-   clean();
-   // TODO allow more arguments
-   // if one argument, assume that to be input file, otherwise use stdin
+	clean();
+   	// if one argument, assume that to be input file, otherwise use stdin
 	if(argc<2){
-		pthread_create(&t_id, NULL,toaud, "/dev/stdin\0");
+		pthread_create(&t_id, NULL,textToAudio, "/dev/stdin\0");
 	}else{
-		pthread_create(&t_id, NULL,toaud, argv[1]);
+		pthread_create(&t_id, NULL,textToAudio, argv[1]);
 	}
-	//todo make  args
-
-	//convert tts
-	//speak tts
-	speak();	
+	//speak();	
 	//done
 	pthread_join(t_id, NULL);
 	printf("Done\n");
--- tests/test.c
+++ tests/test.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #define UNIT_TESTING
 #include "../src/ttsp.c"
-#include <assert.h>
-
 int M_TestReport(int fail, int succ){
    printf("\nRan %d Tests: %d failed, %d succeeded.\n", fail+succ , fail, succ);
    return fail;
@@ -14,25 +12,29 @@ int M_AssertString(char * a, char * b, int* fail, int * succ){
       return 1;
    }
    *fail+=1;
-   printf ("Assert fail: [%s]!=[%s]\n",a,b);
+   printf ("Assert fail: [%s]==[%s]\n",a,b);
    return 0; 
 }
 
 int testStringin(){
-   int  f=0;
-   int  s=0;
-   system("echo \"uwu. owo\n AWO aaa. Can't, cannot\">TESTTXT");
+   int  f=0;int  s=0;
+   system("echo \"uwu. owo\n AWO\" aaa\". Can't, cannot\">TESTTXT");
    char ** t = splitTxt("TESTTXT");
-   assert(t!=NULL);
    M_AssertString(  t[0]  , "uwu"  , &f, &s);
    M_AssertString(  t[1]  , "owo"  , &f, &s );
-   M_AssertString(  t[2]  , "AwO aaa"   , &f, &s);
+   M_AssertString(  t[2]  , "AWO aaa"   , &f, &s);
    M_AssertString(  t[3]  , "Can't"  , &f, &s);
-   //M_AssertString(  t[4]  , "cannot"  , &f, &s );
-   printf("uwu\n");
+   M_AssertString(  t[4]  , "cannot"  , &f, &s );
+   system("rm TESTTXT");
+   return M_TestReport(f, s);
+}
+int testparse(){
+   int  f=0;int  s=0;
+   M_AssertString(  ParseTtsText("uwu")  , "uwu"  , &f, &s);
    return M_TestReport(f, s);
 }
 int main(){
 	int fail=0,succ=0;
 	testStringin() ? fail++ : succ ++;
+	testparse() ? fail++ : succ ++;
 }
